4d7504fd05e09078dc231287b2deb54f408a0273
iali88 <irfan.ali@free.fr>
2020-02-26 19:32:04 +0100

    First commit - Création du projet

---
+++ src/main/java/com/recettes/recettesapp/RecettesAppApplication.java
@@ -0,0 +1,13 @@
+package com.recettes.recettesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecettesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecettesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recettes/recettesapp/RecettesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recettes.recettesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecettesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
